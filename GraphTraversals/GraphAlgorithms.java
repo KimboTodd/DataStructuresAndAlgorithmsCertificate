74d29e2c3141feda832316a85908d39904fe37b0
KimboTodd <kimtoddsotheremail@gmail.com>
2023-08-01 12:26:58 -0400

    prims algorithm

--- GraphTraversals/GraphAlgorithms.java
+++ GraphTraversals/GraphAlgorithms.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
+import java.util.PriorityQueue;
+import java.util.Set;
 
 /**
  * Your implementation of various graph traversal algorithms.
@@ -131,5 +133,90 @@ public class GraphAlgorithms {
                 }
             }
         }
+    }
+
+    /**
+     * Runs Prim's algorithm on the given graph and returns the Minimum
+     * Spanning Tree (MST) in the form of a set of Edges. If the graph is
+     * disconnected and therefore no valid MST exists, return null.
+     *
+     * You may assume that the passed in graph is undirected. In this framework,
+     * this means that if (u, v, 3) is in the graph, then the opposite edge
+     * (v, u, 3) will also be in the graph, though as a separate Edge object.
+     *
+     * The returned set of edges should form an undirected graph. This means
+     * that every time you add an edge to your return set, you should add the
+     * reverse edge to the set as well. This is for testing purposes. This
+     * reverse edge does not need to be the one from the graph itself; you can
+     * just make a new edge object representing the reverse edge.
+     *
+     * You may assume that there will only be one valid MST that can be formed.
+     *
+     * You should NOT allow self-loops or parallel edges in the MST.
+     *
+     * You may import/use java.util.PriorityQueue, java.util.Set, and any
+     * class that implements the aforementioned interface.
+     *
+     * DO NOT modify the structure of the graph. The graph should be unmodified
+     * after this method terminates.
+     *
+     * The only instance of java.util.Map that you may use is the adjacency
+     * list from graph. DO NOT create new instances of Map for this method
+     * (storing the adjacency list in a variable is fine).
+     *
+     * You may assume that the passed in start vertex and graph will not be null.
+     * You may assume that the start vertex exists in the graph.
+     *
+     * @param <T>   The generic typing of the data.
+     * @param start The vertex to begin Prims on.
+     * @param graph The graph we are applying Prims to.
+     * @return The MST of the graph or null if there is no valid MST.
+     */
+    public static <T> Set<Edge<T>> prims(Vertex<T> start, Graph<T> graph) {
+        var mSTedgeSet = new HashSet<Edge<T>>();
+        var visitedSet = new HashSet<Vertex<T>>();
+        var priorityQueue = new PriorityQueue<Edge<T>>();
+
+        var validNumberEdges = (graph.getVertices().size() - 1) * 2;
+        Set<Edge<T>> edges = graph.getEdges();
+
+        for (Edge<T> edge : edges) {
+            // starting with starting vertex
+            if (edge.getU().equals(start)) {
+                priorityQueue.add(edge);
+            }
+        }
+        visitedSet.add(start);
+        while (!priorityQueue.isEmpty() && visitedSet.size() != validNumberEdges) {
+            var poppedEdge = priorityQueue.poll();
+            Vertex<T> destinationVertex = poppedEdge.getV();
+            if (visitedSet.contains(destinationVertex) == false) {
+                visitedSet.add(destinationVertex);
+                mSTedgeSet.add(poppedEdge);
+                // add inverse per java docs
+                mSTedgeSet.add(new Edge<T>(poppedEdge.getV(), poppedEdge.getU(), poppedEdge.getWeight()));
+
+                // loop through edges
+                for (Edge<T> childEdge : edges) {
+                    // loop through the edges looking for a starting edge that matches currentEdge
+                    if (childEdge.getU().equals(destinationVertex)) {
+                        // if we find one, enqueue it only if the destination vertex hasn't been visited
+                        if (!visitedSet.contains(childEdge.getV())) {
+                            priorityQueue.add(childEdge);
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!priorityQueue.isEmpty()) {
+            return null;
+        }
+
+        if (mSTedgeSet.size() != validNumberEdges) {
+            return null;
+        }
+
+        return mSTedgeSet;
     }
 }
--- GraphTraversals/GraphAlgorithmsTest.java
+++ GraphTraversals/GraphAlgorithmsTest.java
@@ -110,7 +110,112 @@ public class GraphAlgorithmsTest {
         edges.add(new Edge<>(new Vertex<>(4), new Vertex<>(4), 1)); // vertex 4 is connected to itself
         Graph<Integer> graph = new Graph<>(vertices, edges);
         List<Vertex<Integer>> dfsResult = GraphAlgorithms.dfs(new Vertex<>(0), graph);
-        Assert.assertEquals(List.of(new Vertex<>(0), new Vertex<>(1), new Vertex<>(2), new Vertex<>(3), new Vertex<>(4)),
+        Assert.assertEquals(
+                List.of(new Vertex<>(0), new Vertex<>(1), new Vertex<>(2), new Vertex<>(3), new Vertex<>(4)),
                 dfsResult);
+    }
+
+    @Test
+    public void testPrims() {
+        Set<Vertex<Integer>> vertices = new HashSet<>();
+        for (int i = 0; i < 5; i++) {
+            vertices.add(new Vertex<>(i));
+        }
+        Set<Edge<Integer>> edges = new HashSet<>();
+        edges.add(new Edge<>(new Vertex<>(0), new Vertex<>(1), 1));
+        edges.add(new Edge<>(new Vertex<>(0), new Vertex<>(2), 3));
+        edges.add(new Edge<>(new Vertex<>(1), new Vertex<>(2), 1));
+        edges.add(new Edge<>(new Vertex<>(1), new Vertex<>(3), 4));
+        edges.add(new Edge<>(new Vertex<>(2), new Vertex<>(3), 2));
+        edges.add(new Edge<>(new Vertex<>(2), new Vertex<>(4), 5));
+        edges.add(new Edge<>(new Vertex<>(3), new Vertex<>(4), 6));
+        Graph<Integer> graph = new Graph<>(vertices, edges);
+        Set<Edge<Integer>> mst = GraphAlgorithms.prims(new Vertex<>(0), graph);
+        Set<Edge<Integer>> expectedMst = new HashSet<>();
+        expectedMst.add(new Edge<>(new Vertex<>(0), new Vertex<>(1), 1));
+        expectedMst.add(new Edge<>(new Vertex<>(1), new Vertex<>(2), 1));
+        expectedMst.add(new Edge<>(new Vertex<>(2), new Vertex<>(3), 2));
+        expectedMst.add(new Edge<>(new Vertex<>(2), new Vertex<>(4), 5));
+        Assert.assertEquals(expectedMst, mst);
+    }
+
+    @Test
+    public void testPrimsSingleVertex() {
+        Set<Vertex<Integer>> vertices = new HashSet<>();
+        vertices.add(new Vertex<>(0));
+        Graph<Integer> graph = new Graph<>(vertices, new HashSet<>());
+        Set<Edge<Integer>> mst = GraphAlgorithms.prims(new Vertex<>(0), graph);
+        Set<Edge<Integer>> expectedMst = new HashSet<>();
+        Assert.assertEquals(expectedMst, mst);
+    }
+
+    @Test
+    public void testPrimsDisconnectedGraphNull() {
+        Set<Vertex<Integer>> vertices = new HashSet<>();
+        for (int i = 0; i < 5; i++) {
+            vertices.add(new Vertex<>(i));
+        }
+        Set<Edge<Integer>> edges = new HashSet<>();
+        edges.add(new Edge<>(new Vertex<>(0), new Vertex<>(1), 1));
+        edges.add(new Edge<>(new Vertex<>(1), new Vertex<>(2), 1));
+        edges.add(new Edge<>(new Vertex<>(3), new Vertex<>(4), 1));
+        Graph<Integer> graph = new Graph<>(vertices, edges);
+        Set<Edge<Integer>> mst = GraphAlgorithms.prims(new Vertex<>(0), graph);
+        Assert.assertNull(mst);
+    }
+
+    @Test
+    public void testPrimsParallelEdges() {
+        Set<Vertex<Integer>> vertices = new HashSet<>();
+        for (int i = 0; i < 5; i++) {
+            vertices.add(new Vertex<>(i));
+        }
+        Set<Edge<Integer>> edges = new HashSet<>();
+        edges.add(new Edge<>(new Vertex<>(0), new Vertex<>(1), 1));
+        edges.add(new Edge<>(new Vertex<>(0), new Vertex<>(2), 3));
+        edges.add(new Edge<>(new Vertex<>(1), new Vertex<>(2), 1));
+        edges.add(new Edge<>(new Vertex<>(1), new Vertex<>(3), 4));
+        edges.add(new Edge<>(new Vertex<>(2), new Vertex<>(3), 2));
+        edges.add(new Edge<>(new Vertex<>(2), new Vertex<>(4), 5));
+        edges.add(new Edge<>(new Vertex<>(3), new Vertex<>(4), 6));
+        edges.add(new Edge<>(new Vertex<>(1), new Vertex<>(2), 2));
+        Graph<Integer> graph = new Graph<>(vertices, edges);
+        Set<Edge<Integer>> mst = GraphAlgorithms.prims(new Vertex<>(0), graph);
+        Set<Edge<Integer>> expectedMst = new HashSet<>();
+        expectedMst.add(new Edge<>(new Vertex<>(0), new Vertex<>(1), 1));
+        expectedMst.add(new Edge<>(new Vertex<>(1), new Vertex<>(2), 1));
+        expectedMst.add(new Edge<>(new Vertex<>(2), new Vertex<>(3), 2));
+        expectedMst.add(new Edge<>(new Vertex<>(2), new Vertex<>(4), 5));
+        Assert.assertEquals(expectedMst, mst);
+    }
+
+    @Test
+    public void testPrimsNoMST() {
+        Set<Vertex<Integer>> vertices = new HashSet<>();
+        for (int i = 0; i < 5; i++) {
+            vertices.add(new Vertex<>(i));
+        }
+        Set<Edge<Integer>> edges = new HashSet<>();
+        edges.add(new Edge<>(new Vertex<>(0), new Vertex<>(1), 1));
+        edges.add(new Edge<>(new Vertex<>(1), new Vertex<>(2), 1));
+        edges.add(new Edge<>(new Vertex<>(2), new Vertex<>(3), 1));
+        edges.add(new Edge<>(new Vertex<>(3), new Vertex<>(4), 1));
+        Graph<Integer> graph = new Graph<>(vertices, edges);
+        Set<Edge<Integer>> mst = GraphAlgorithms.prims(new Vertex<>(0), graph);
+        Assert.assertNull(mst);
+    }
+
+    @Test
+    public void testPrimsTwoVertices() {
+        Set<Vertex<Integer>> vertices = new HashSet<>();
+        vertices.add(new Vertex<>(0));
+        vertices.add(new Vertex<>(1));
+        Set<Edge<Integer>> edges = new HashSet<>();
+        edges.add(new Edge<>(new Vertex<>(0), new Vertex<>(1), 1));
+        Graph<Integer> graph = new Graph<>(vertices, edges);
+        Set<Edge<Integer>> mst = GraphAlgorithms.prims(new Vertex<>(0), graph);
+        Set<Edge<Integer>> expectedMst = new HashSet<>();
+        expectedMst.add(new Edge<>(new Vertex<>(0), new Vertex<>(1), 1));
+        Assert.assertEquals(expectedMst, mst);
     }
 }
